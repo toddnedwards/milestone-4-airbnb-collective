c8ad35ee611dc29dea318fa43c15ccc147f69e0a
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-06-30 15:28:21 +0000

    properties views.py and urls.py added

--- myairbnb_collective/urls.py
+++ myairbnb_collective/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('properties/', include('properties.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls import path
+from . import views
 
 # Create your views here.
+
+urlpatterns = [
+    path('/properties', views.properties, name='properties'),
+]
