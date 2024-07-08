64ef4213332e578eb1ea69113bb82212521e8bc4
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-07-08 13:39:12 +0000

    properties json file updated with facilities. migrated

--- airbnb_properties/models.py
+++ airbnb_properties/models.py
@@ -7,11 +7,16 @@ class Property(models.Model):
     name = models.CharField(max_length=254)
     location = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
+    full_description = models.TextField(null=True, blank=True)
     price_per_night = models.DecimalField(max_digits=6, decimal_places=2)
     availability = models.CharField(max_length=50, null=True, blank=True)
     distance_to_airport = models.IntegerField(null=True, blank=True)
     bedrooms = models.IntegerField(null=True, blank=True)
+    has_wifi = models.BooleanField(default=False)
+    has_parking = models.BooleanField(default=False)
+    non_smoking = models.BooleanField(default=False)
     image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
