9d24bfbe89b18cdd303d50e97033d1772f245471
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-07-17 16:06:21 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PdZpJFZHmSDqVxQZj4THNgKhCgZvccY7ktOjLbzsCFYVD6IxAFVCmhxB3xYNXeR3wdQHUZxXoryX8QODUo7QbKx00j4EUMAEp',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
