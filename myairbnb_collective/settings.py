cb244409a9dd6479d025ff1e13dbf6eab487086c
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-07-26 12:35:53 +0000

    auth.account.middleware removed from settings for compatibility

--- myairbnb_collective/settings.py
+++ myairbnb_collective/settings.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'myairbnb_collective.urls'
