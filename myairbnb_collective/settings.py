6d9d31f035ae7e28d28a95e30de0538566f64443
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-07-27 12:53:44 +0000

    debug changed to true

--- myairbnb_collective/settings.py
+++ myairbnb_collective/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+# 
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['my-airbnb-collective-57b00b515cab.herokuapp.com', 'localhost']
 
