efc5a59df1266ecd311101666d5628c2d89aff55
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-07-29 15:37:47 +0000

    contact form function added

--- contact/views.py
+++ contact/views.py
@@ -10,10 +10,18 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            return redirect('success')
+            send_mail(
+                form.cleaned_data['subject'],
+                f"Message from {form.cleaned_data['name']} <{form.cleaned_data['email']}>\n\n"
+                f"{form.cleaned_data['message']}",
+                None,
+                ['myairbnbcollective@gmail.com'],
+            )
+            return render(request, 'success.html')
     else:
         form = ContactForm()
-    return render(request, 'contact.html', {'form': form})
+    
+    return render(request, 'contact_us.html', {'form': form})
 
 def success(request):
     return render(request, 'success.html')
