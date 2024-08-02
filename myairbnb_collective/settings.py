9aec4ccf83f9b149f2335a7dd3618457d886c81e
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-08-02 16:01:37 +0000

    env.py added

--- myairbnb_collective/settings.py
+++ myairbnb_collective/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -176,6 +178,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
 
 USE_L10N = True
