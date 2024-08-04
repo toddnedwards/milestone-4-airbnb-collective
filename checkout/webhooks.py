1e8de945c0582954fabb26c7aba008e61abd24a7
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-08-04 15:49:53 +0000

    update webhooks now successful

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -14,6 +14,7 @@ def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
     wh_secret = settings.STRIPE_WH_SECRET
+    print('secret in webhooks.py', wh_secret)
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     # Get the webhook data and verify its signature
@@ -28,11 +29,11 @@ def webhook(request):
     except ValueError as e:  # NOQA
         # Invalid payload
         print('Invalid payload')
-        return HttpResponse(status=400)
+        return HttpResponse(content=e, status=400)
     except stripe.error.SignatureVerificationError as e:  # NOQA
         # Invalid signature
         print('Invalid signature')
-        return HttpResponse(status=400)
+        return HttpResponse(content=e, status=400)
     except Exception as e:
         print('General exception')
         return HttpResponse(content=e, status=400)
@@ -43,9 +44,7 @@ def webhook(request):
     # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': (
-            handler.handle_payment_intent_payment_failed
-        ),
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
     # Get the webhook type from Stripe
