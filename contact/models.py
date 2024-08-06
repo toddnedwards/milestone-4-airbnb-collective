23a550a29992b1642c01ba013b792b9de0a955c0
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-08-06 08:14:39 +0000

    models created and migrated for contact form

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    message = models.TextField(max_length=255, null=False, blank=False)
