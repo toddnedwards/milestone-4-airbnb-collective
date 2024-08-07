bbab6fe56fa22fb28dc9be1522726c0032ff1c87
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-08-07 22:36:22 +0000

    python views backslash removed

--- checkout/views.py
+++ checkout/views.py
@@ -30,8 +30,9 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
+        messages.error(
+            request, 'Sorry, your payment cannot be processed right now.'
+                     'Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
