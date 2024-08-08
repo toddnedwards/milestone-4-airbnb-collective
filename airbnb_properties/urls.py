78d814a6d9f5a1061a16e3c4e68e1dea4e303fed
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-08-08 10:43:23 +0000

    add property created

--- airbnb_properties/urls.py
+++ airbnb_properties/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('properties/', views.airbnb_properties, name='properties'),
-    path('<property_id>/', views.property_details, name='property_details'),
-    path('edit/<str:property_id>/', views.edit_property, name='edit_property'),
-    path('delete/<property_id>/', views.delete_property,
-         name='delete_property'),
+    path('add/', views.add_property, name='add_property'),
+    path('<int:property_id>/', views.property_details, name='property_details'),
+    path('edit/<int:property_id>/', views.edit_property, name='edit_property'),
+    path('delete/<int:property_id>/', views.delete_property, name='delete_property'),
 ]
