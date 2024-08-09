05afddcc9323cafc3aeceac1080cdeabb1c57e4d
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-08-09 18:28:09 +0000

    placeholders added to property forms for admin clarification

--- airbnb_properties/forms.py
+++ airbnb_properties/forms.py
@@ -8,13 +8,43 @@ from .models import Property
 class PropertyForm(forms.ModelForm):
 
     guest_count = forms.ChoiceField(
-        label='Guest Count', choices=[(i, i) for i in range(1, 20)])
-
-    image = forms.ImageField(label='Image', required=False)
+        label='Guest Count', choices=[(i, i) for i in range(1, 20)],
+        required=False
+    )
 
     class Meta:
         model = Property
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(PropertyForm, self).__init__(*args, **kwargs)
+
+        placeholders = {
+            'category': 'Category',
+            'name': 'Name',
+            'location': 'Location',
+            'description': 'Short Description',
+            'full_description': 'Full Description',
+            'price_per_night': 'Price Per Night (£)',
+            'distance_to_airport': 'Distance To Airport',
+            'bedrooms': 'Number Of Bedrooms',
+            'has_wifi': 'Has Wifi?',
+            'has_parking': 'Has Parking?',
+            'non_smoking': 'Non Smoking?',
+            'pet_friendly': 'Pet Friendly?',
+            'image': 'Main Image',
+            'image_kitchen': 'Image Kitchen',
+            'image_bathroom': 'Image Bathroom',
+            'total_days': 'Total Days',
+        }
+
+        for field_name, placeholder in placeholders.items():
+            if field_name in self.fields:
+                self.fields[field_name].widget.attrs.update({
+                    'placeholder': placeholder
+                })
+
+    image = forms.ImageField(label='Image', required=False)
 
 
 class DateRangeInput(forms.DateInput):
